ecbf5ace58a15ecc6337798de93bec24570d9f97
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2016-08-15 14:44:30 -0400

    Refactored

--- src/message.js
+++ src/message.js
@@ -1,19 +1,6 @@
 'use strict'
 
-/**
- * Prints stylized type names for values (or arrays of values) passed.
- * @function
- * @param {*} val
- * @return {String}
- */
-function printType(val, allowArray) {
-  if (allowArray && Array.isArray(val)) {
-    return val.map(inVal => printType(inVal, true)).join('|')
-  }
-  if (val === null) return 'null'
-  if (typeof val === 'undefined') return 'undefined'
-  return val.name || val.constructor.name
-}
+const getSpecificType = require('./getSpecificType')
 
 /**
  * Generates a string representing what was required and what was passed.
@@ -26,13 +13,13 @@ module.exports = function(required, passed) {
   let message = '\n\n  REQUIRED:   '
   if (!required.length) message += 'void'
   for (let i = 0; i < required.length; i++) {
-    message += printType(required[i], true)
+    message += getSpecificType(required[i], true)
     if (i < required.length - 1) message += ', '
   }
   message += '\n  PASSED:     '
   if (!passed.length) message += 'void'
   for (let i = 0; i < passed.length; i++) {
-    message += printType(passed[i])
+    message += getSpecificType(passed[i])
     if (i < passed.length - 1) message += ', '
   }
   return message + '\n'
