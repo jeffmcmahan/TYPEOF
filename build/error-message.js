1d49e6fe906f611cf59b2a41a68c6a9093825c61
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2016-08-19 16:28:54 -0400

    Latest

--- build/error-message.js
+++ build/error-message.js
@@ -1,26 +1,12 @@
 'use strict'
 
 var printSpecificType = require('./print-specific-type')
+var utils = require('./utils')
 var is = require('./is')
+var IDENT = '@@@@@@@@@@@'
 
 /**
- *
- * @function
- * @param {Object} obj
- * @return {String}
- */
-function prettyJSON(obj) {
-  return (
-    JSON.stringify(obj)
-      .replace(/"/g, '')
-      .replace(/,/g, ', ')
-      .replace(/{/g, '{ ')
-      .replace(/}/g, ' }')
-  )
-}
-
-/**
- *
+ * Produces a JSON representation of duck-type requirement.
  * @function
  * @param {Array|Object} rq
  * @return {String}
@@ -30,11 +16,12 @@ function printDuckType(rq) {
   Object.keys(rq).forEach(function (key) {
     obj[key] = printSpecificType(rq[key])
   })
-  return prettyJSON(obj)
+  return utils.prettyJSON(obj)
 }
 
 /**
- *
+ * Produces a JSON representation of duck-typed argument (omits properties not
+ * included in the duck-type).
  * @function
  * @param {Array|Object} args
  * @param {Array|Object} rq
@@ -45,11 +32,11 @@ function printDuckTypedArg(arg, rq) {
   Object.keys(rq).forEach(function (key) {
     obj[key] = printSpecificType(arg[key])
   })
-  return prettyJSON(obj)
+  return utils.prettyJSON(obj)
 }
 
 /**
- *
+ * Produces an array containing a string describing each required type.
  * @function
  * @param {Array|Object} rq
  * @return {Array}
@@ -81,7 +68,7 @@ function getRqTypes(rq) {
 }
 
 /**
- *
+ * Produces and array of strings describing the type of each argument.
  * @function
  * @param {Array|Object} args
  * @param {Array|Object} rq
@@ -99,102 +86,109 @@ function getArgTypes(args, rq) {
   return argTypes
 }
 
-
-function chars(count, char) {
-  var str = ''
-  for (var i = 0; i < count; i++) {
-    str += char || ' '
-  }
-  return str
-}
-
-
-function isSimpleTypeRQ(rqWord) {
-  if (typeof rqWord === 'undefined') return true
-  return (
-    rqWord !== '*' &&
-    rqWord.indexOf('|') === -1
-  )
-}
-
+/**
+ * Determines whether a given requirement string describes a disjoint rq.
+ * @function
+ * @param {Stirng} rqWord
+ * @return {Boolean}
+ */
 function isDisjointRQ(rqWord) {
   return rqWord.indexOf('|') !== -1
 }
 
+/**
+ * Returns false if the requirement is disjoint or kleene star.
+ * @function
+ * @param {Stirng} rqWord
+ * @return {Boolean}
+ * @todo Rename this; it's not quite right.
+ */
+function isSimpleTypeRQ(rqWord) {
+  if (typeof rqWord === 'undefined') return true
+  return (rqWord !== '*' && !isDisjointRQ(rqWord))
+}
+
+/**
+ * Finds each point of character-to-character difference between arg and rq
+ * strings; inserts carets to indicate divergences.
+ * @function
+ * @param {Array} args
+ * @param {Array} rq
+ * @return {Array}
+ */
 function diff(args, rq) {
   var theDiff = []
-  var count = getLongest(args, rq)
+  var count = utils.getLongest(args, rq)
   for (var i = 0; i < count; i++) {
     var rqWord = rq[i] || ''
     var argWord = args[i] || ''
     if (isSimpleTypeRQ(rqWord)) {
       if (rqWord !== argWord) {
-        theDiff.push(chars(argWord.length, '^'))
+        theDiff.push(utils.chars(argWord.length - 2, '^') + utils.chars(2))
       } else {
-        theDiff.push(chars(argWord.length, ' '))
+        theDiff.push(utils.chars(argWord.length, ' '))
       }
     } else if (isDisjointRQ(rqWord)) {
       if (rqWord.indexOf(argWord) !== -1) {
-        theDiff.push(chars(argWord.length, ' '))
+        theDiff.push(utils.chars(argWord.length, ' '))
       } else {
-        theDiff.push(chars(argWord.length, '^'))
+        theDiff.push(utils.chars(argWord.length, '^'))
       }
     } else {
-      theDiff.push(chars(argWord.length, ' '))
+      theDiff.push(utils.chars(argWord.length, ' '))
     }
   }
   return theDiff
 }
 
-
-function getLongest() {
-  var arguments$1 = arguments;
-
-  var longest = 0
-  for (var i = 0; i < arguments.length; i++) {
-    if (!is.array(arguments$1[i]) && !is.string(arguments$1[i])) continue
-    if (arguments$1[i].length > longest) longest = arguments$1[i].length
+/**
+ * Renders arrays of strings in soft-tab-delimited columns.
+ * @function
+ * @param {Array} argTypes
+ * @param {Array} rqTypes
+ * @param {Array} theDiff
+ * @return {String}
+ */
+function columnize(argTypes, rqTypes, theDiff) {
+  var colWidths = []
+  for (var i = 0; i < argTypes.length; i++) {
+    argTypes[i] = argTypes[i] || ''
+    rqTypes[i] = rqTypes[i] || ''
+    theDiff[i] = theDiff[i] || ''
+    colWidths[i] = utils.getLongest(argTypes[i], rqTypes[i], theDiff[i])
   }
-  return longest
-}
-
-function addDelimiter(arr, char) {
-  return arr.map(function (item, index) {
-    if (index !== arr.length - 1) return item + char
-    return item
-  })
-}
-
-function padArrays() {
-  var arguments$1 = arguments;
-
-  var length = getLongest.apply(null, arguments)
-  for (var i = 0; i < arguments.length; i++) {
-    for (var j = 0; j < length; j++) {
-      if (typeof arguments$1[i][j] === 'undefined') arguments$1[i][j] = ''
-    }
-  }
-}
-
-function normalize(argTypes, rqTypes, theDiff) {
   return function (input) {
     for (var i = 0; i < argTypes.length; i++) {
-      argTypes[i] = argTypes[i] || ''
-      rqTypes[i] = rqTypes[i] || ''
-      theDiff[i] = theDiff[i] || ''
-      var length = getLongest(argTypes[i], rqTypes[i], theDiff[i])
-      if (argTypes[i].length < length) {
-        argTypes[i] += chars(length - argTypes[i].length, ' ')
+      input[i] = input[i] || ''
+      if (input[i].length < colWidths[i]) {
+        input[i] += utils.chars(colWidths[i] - input[i].length, ' ')
       }
-      if (rqTypes[i].length < length) {
-        rqTypes[i] += chars(length - rqTypes[i].length, ' ')
-      }
-      if (theDiff[i].length < length) {
-        theDiff[i] += chars(length - theDiff[i].length, ' ')
+      if (input[i].length > colWidths[i]) {
+        input[i] = input[i].slice(0, colWidths[i])
       }
     }
-    return input
+    return utils.trimComma(input.join(' '))
   }
+}
+
+/**
+ * Creates an array with bad values at the appropriate indices.
+ * @function
+ * @param {Array} violations
+ * @param {Number} length
+ * @return {Array}
+ */
+function badValues(violations, length) {
+  var valuesLine = []
+  valuesLine.length = length
+  violations.forEach(function (violation) {
+    if (typeof violation.v !== 'undefined') {
+      valuesLine[violation.n] = utils.prettyJSON.tight(violation.v)
+    } else {
+      valuesLine[violation.n] = 'undefined'
+    }
+  })
+  return valuesLine
 }
 
 /**
@@ -205,15 +199,40 @@ function normalize(argTypes, rqTypes, theDiff) {
  * @param {Array|Object} rq
  * @return {String}
  */
-module.exports = function(args, rq) {
-  var rqTypes = addDelimiter(getRqTypes(rq), ',')
-  var argTypes = addDelimiter(getArgTypes(args, rq), ',')
+function errMsg(args, rq, violations) {
+  var rqTypes = utils.addDelimiter(getRqTypes(rq), ', ')
+  var argTypes = utils.addDelimiter(getArgTypes(args, rq), ', ')
   var theDiff = diff(argTypes, rqTypes)
-  var normalizer = normalize(argTypes, rqTypes, theDiff)
-  return (
-    '\n' +
-    '  Required:  ' + normalizer(rqTypes).join(' ') + '\n'+
-    '  Provided:  ' + normalizer(argTypes).join(' ') + '\n'+
-    '             ' + normalizer(theDiff).join(' ')
-  )
+  var theValues = badValues(violations, theDiff.length)
+  var columnizer = columnize(argTypes, rqTypes, theDiff)
+  return (("\n\n    Required:  " + (columnizer(rqTypes)) + "\n    Provided:  " + (columnizer(argTypes)) + "\n               " + (columnizer(theDiff)) + "\n               " + (columnizer(theValues)) + "\n    " + IDENT))
 }
+
+/**
+ * Removes the stack item which refers to the TYPEOF function.
+ * @function
+ * @param {String} stack
+ * @return {String}
+ */
+errMsg.filterStack = function (stack) {
+  stack = stack.split(IDENT)
+  var top = stack[0].replace('TypeError', 'TypeError thrown by TYPEOF')
+  var bottom = (
+    stack[1]
+      .trim().split('\n').slice(1)
+      .map(function (str) { return '    ' + str.trim(); }).join('\n')
+  )
+  return top + '\n' + bottom
+}
+
+errMsg.NO_ARGS = (
+  'TYPEOF requires a single array-like argument: \n'+
+  '  TYPEOF(<array-like>)(type1, type2, ...)\n'
+)
+
+errMsg.NO_RQ = (
+  'TYPEOF requires explicit declarations. Use "void" to indicate that no '+
+  'values are permitted; use "*" to permit anything.'
+)
+
+module.exports = errMsg
