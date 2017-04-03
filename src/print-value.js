88bfad97206e8e0574640838b01182fb9573864f
Jeff McMahan <jeffrey.lee.mcmahan@gmail.com>
2017-04-03 11:52:11 -0400

    Added comments

--- src/print-value.js
+++ src/print-value.js
@@ -1,5 +1,13 @@
 'use strict'
 
+const NATIVE_TYPES = ['String', 'Array', 'Boolean', 'Number', 'Function']
+
+/**
+ * Generates a string to describe the type of the given value.
+ * @param {*} value
+ * @param {Boolean} isRQ
+ * @return {String}
+ */
 function printType(value, isRQ = false) {
   if (value === 'void') return 'void'
   if (value === null) return 'null'
@@ -17,58 +25,68 @@ function printType(value, isRQ = false) {
   }
 }
 
+/**
+ * Generates a string representation of the given Object.
+ * @param {Object} value
+ * @return {String}
+ */
 function printObject(value) {
   const keys = Object.keys(value).filter(key => value[key] !== value)
   const obj= {}
   keys.forEach(key => obj[key] = printType(value[key]))
   if (!keys.length) return '{}'
-  return (
-    JSON.stringify(obj, null, 2)
-      .replace(/\s+/g, ' ')
-      .replace(/:\s/g, ':')
-      .replace('{', '{')
-      .replace('}', '... }')
-      .trim()
-  )
+  return JSON.stringify(obj, null, 2)
+    .replace(/\s+/g, ' ')
+    .replace(/:\s/g, ':')
+    .trim()
 }
 
-function printArray(value) {
-  if (!value.length) return '[]'
-  return (
-    '[ ' +
-    value.slice(0,3)
-      .map(printType)
-      .join(', ') +
-    ' ]'
-  )
+/**
+ * Generates a string representation of the given Array value.
+ * @param {Array} arr
+ * @return {String}
+ */
+function printArray(arr) {
+  if (!arr.length) return '[]'
+  return `[ ${arr.slice(0,3).map(printType).join(', ')} ]`
 }
 
-function printDuckType(rq) {
-  const keys = Object.keys(rq)
+/**
+ * Generates a string representation of the given duck type requirement.
+ * @param {Object} rq
+ * @return {String}
+ */
+function printDuckType(duckType) {
+  const keys = Object.keys(duckType)
   const obj = {}
-  keys.forEach(key => obj[key] = exports.rq(rq[key]))
+  keys.forEach(key => obj[key] = exports.rq(duckType[key]))
   if (!keys.length) return '{}'
-  return (
-    JSON.stringify(obj, null, 2)
-      .replace(/\s+/g, ' ')
-      .replace(/:\s/g, ':')
-      .replace('{', '{')
-      .replace('}', '... }')
-      .trim()
-  )
+  return JSON.stringify(obj, null, 2)
+    .replace(/\s+/g, ' ')
+    .replace(/:\s/g, ':')
+    .trim()
 }
 
-function printDisjoint(rq) {
-  return rq.map(exports.rq).join('|')
+/**
+ * Generates a string to describe the given disjoint type description.
+ * @param {*} rq
+ * @return {String}
+ */
+function printDisjoint(disjointDesc) {
+  return disjointDesc.map(exports.rq).join('|')
 }
 
+/**
+ * Generates a string to describe the given requirment.
+ * @param {*} rq
+ * @return {String}
+ */
 exports.rq = function (rq) {
+  if (rq === '__VOID') return 'void (implicit)'
   if (rq === 'void') return 'void'
   if (typeof rq === 'function') {
-    if (['String', 'Array', 'Boolean', 'Number', 'Function'].indexOf(rq.name) !== -1) {
-      return rq.name.toLowerCase()
-    }
-    return rq.name
+    if (NATIVE_TYPES.indexOf(rq.name) !== -1) return rq.name.toLowerCase()
+    return (rq.name || rq.__NAME)
   }
   if (typeof rq === 'undefined') return 'undefined'
   if (rq === null) return 'null'
@@ -77,7 +95,13 @@ exports.rq = function (rq) {
   if (typeof rq === 'string') return rq
 }
 
+/**
+ * Generates a string to describe the given value.
+ * @param {*} arg
+ * @return {String}
+ */
 exports.arg = function (arg) {
+  if (arg === '__VOID') return 'void'
   if (typeof arg === 'undefined') return 'undefined'
   if (arg === null) return 'null'
   if (typeof arg === 'object') {
